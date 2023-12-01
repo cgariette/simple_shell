0111e14a2bd234315c8ac85d40eb28271bba3077
fadhilijane <janefadhili@gmail.com>
2023-12-01 07:52:53 +0000

    ss

--- shell.c
+++ shell.c
@@ -8,20 +8,47 @@ void _prompt(void)
 {
 	size_t n = 10;
 	char * buffer = NULL;
+	ssize_t line;
 
 	_putchar('$');
+	line = getline(&buffer, &n, stdin);
 
-	getline(&buffer, &n, stdin);
+	if (line == -1)
+	{
+		perror("Error");
+		free(buffer);
+		exit(EXIT_FAILURE);
+	}
 
 	free (buffer);
 
 }
 
+void _parse(char *str, char **args)
+{
+	char *token;
+	int count;
+
+	token = strtok(str, " "); 
+
+	while(token)
+	{
+		args[count++] = token;
+		printf("%s\n", token);
+		token = strtok(NULL, " ");
+	}
+	args[count] = NULL;
+}
+
+
 int main()
 {
+	char *args[50];
+
 	while (1)
 	{
 		_prompt();
+		_parse("simple shell", args);
 	}
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int _putchar(char c);
 
