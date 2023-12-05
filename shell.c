a41874b9d8f8de33eb76d28e985f204fa0555ab1
fadhilijane <janefadhili@gmail.com>
2023-12-05 12:22:15 +0000

    shell

--- shell.c
+++ shell.c
@@ -5,7 +5,9 @@ void _parse(char *str, char *delim)
 	char *token;
 	int count = 0;
 	char *string = malloc(sizeof(char) * strlen(str));
-	
+	char args[];
+	int i = 0;
+
 	strcpy(string, str);
 	token = strtok(string, delim); 
 
@@ -17,6 +19,17 @@ void _parse(char *str, char *delim)
 	}
 	count++;
 
+	args = malloc(sizeof(char)* count);
+
+	while(token)
+	{
+		args[i] = malloc(sizeof(char)* strlen(token));
+		strcpy(args[i], token);
+		i++;
+
+		token= strtok(NULL, delim);
+	}
+	args[i] = NULL;
 
 }
 
