2140fb527c227bb4a21c863db8d12c8e53d4a638
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-12-06 15:27:12 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,13 +1,25 @@
 #include "shell.h"
 
-void _parse(char *str, char *delim)
+int main(int argc, char **argv[])
 {
-	char *token;
-	int count = 0;
+	size_t n = 0;
+	char *token, *str, *buffer = NULL;
 	char *string = malloc(sizeof(char) * strlen(str));
-	char args[];
-	int i = 0;
+	int i = 0, count = 0;
+        ssize_t line;
+	char *delim = " \n";
+	
+	while (1)
+	{
+		_putchar('$');
+        	line = getline(&buffer, &n, stdin);
 
+        	if (line == -1)
+        	{
+                	perror("Error");
+                	free(buffer);
+                	exit(EXIT_FAILURE);
+		}
 	strcpy(string, str);
 	token = strtok(string, delim); 
 
@@ -19,41 +31,17 @@ void _parse(char *str, char *delim)
 	}
 	count++;
 
-	args = malloc(sizeof(char)* count);
+	argv = malloc(sizeof(char)* count);
 
-	while(token)
+	while(token)	
 	{
-		args[i] = malloc(sizeof(char)* strlen(token));
-		strcpy(args[i], token);
+		argv[i] = malloc(sizeof(char)* strlen(token));
+		strcpy(argv[i], token);
 		i++;
-
 		token= strtok(NULL, delim);
 	}
-	args[i] = NULL;
+	argv[i] = NULL;
 
-}
-
-
-int main(int argc, char **argv[])
-{
-
-	size_t n = 0;
-        char * buffer = NULL;
-        ssize_t line;
-
-	while (1)
-	{
-		_putchar('$');
-        	line = getline(&buffer, &n, stdin);
-
-        	if (line == -1)
-        	{
-                	perror("Error");
-                	free(buffer);
-                	exit(EXIT_FAILURE);
-        	}
-
-        	_parse(buffer," \n");
 	}
 	free(buffer);
         return (0);
