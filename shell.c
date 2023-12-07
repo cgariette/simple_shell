2325dea76d2576f4ca30af74a9b210f6fec967d8
fadhilijane <janefadhili@gmail.com>
2023-12-07 11:05:32 +0000

    shell repair

--- shell.c
+++ shell.c
@@ -1,50 +1,51 @@
 #include "shell.h"
 
-int main(int argc, char **argv)
+int main(void)
 {
+	int argc = 0, count = 0;
 	size_t n = 0;
-	char *token, *str, *buffer = NULL;
-	char *string = malloc(sizeof(char) * strlen(str));
-	int i = 0, count = 0;
         ssize_t line;
+	char *str = NULL, *string = NULL, *token = NULL;
 	char *delim = " \n";
-	
+	char **argv = NULL;
+
 	while (1)
 	{
 		_putchar('$');
-        	line = getline(&buffer, &n, stdin);
+        	line = getline(&str, &n, stdin);
 
         	if (line == -1)
         	{
                 	perror("Error");
-                	free(buffer);
-                	exit(EXIT_FAILURE);
+                	free(str);
+                	return (-1);
 		}
-	strcpy(string, str);
-	token = strtok(string, delim); 
+		string = strdup(str);
+		token = strtok(str, delim); 
 
-	while(token)
-	{
-		count++;
-		token = strtok(NULL, delim);
+		while(token)
+		{
+			printf("%s\n", token);
+			token = strtok(NULL, delim);
+			argc++;
+		}
+
+		printf(">>>>> %d \n", argc);
+
+		argv = malloc(sizeof(char *) * argc);
+		token = strtok(string, delim);
+
+		while(token)	
+		{
+			argv[count] = token;
+			printf(">>>>> %s \n", argv[count]);
+			count++;
+			token= strtok(NULL, delim);
+		}
+		argv[count] = NULL;
+
 	}
-	count++;
-
-	printf(">>>>> %d \n", count);
-
-	argv = malloc(sizeof(char *)* count);
-
-	while(token)	
-	{
-		argv[i] = malloc(sizeof(char)* strlen(token));
-		strcpy(argv[i], token);
-		i++;
-		printf(">>>>> %s \n", argv[i]);
-		token= strtok(NULL, delim);
-	}
-	argv[i] = NULL;
-
-	}
-	free(buffer);
+	
+	free(str), free(string), free(argv);
         return (0);
 }
