e3eec79f4481192f89b295cbe65105c4d6fe6367
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-12-14 05:59:02 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -10,14 +10,7 @@ void _execute(char **argv)
 {
 	pid_t pid;
 	int val, status, argc;
-	/*char  **env;*/
-	pid = fork();
-	if (pid == 0)
-	{
-		/*env = _getpath(argc, argv);*/
-		val = execve(argv[0], argv, NULL);
-		int val, status;
-		extern char **environ;
+	extern char **environ;
 
 	pid = fork();
 	if (pid == 0)
